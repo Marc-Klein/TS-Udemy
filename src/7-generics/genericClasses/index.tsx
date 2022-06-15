6cf75e4404054423ef88f7acf2663003202f70e6
Marc Klein <maklein@abatgroup.de>
2022-06-15 09:56:41 +0200

    added some new Types

--- src/7-generics/genericClasses/index.tsx
+++ src/7-generics/genericClasses/index.tsx
@@ -1,7 +1,13 @@
+import { type } from "os";
 import React from "react";
 
 const GenericClasses = () => {
-	class DataStorage<T> {
+	// union types can only be defined through the "type" keyword
+	type primitives = string | number | boolean;
+
+	// In Typescript we should only work with primitive types and not reference Types like
+	// objects or arrays
+	class DataStorage<T extends primitives> {
 		private data: T[] = [];
 
 		addItem(item: T) {
@@ -9,6 +15,11 @@ const GenericClasses = () => {
 		}
 
 		removeItem(item: T) {
+			// this if statement elimantes the normal JavaScript behaviour, that if it cant
+			// find soemthing, it returns nothing instead of the last char of the array "-1"
+			if (this.data.indexOf(item) === -1) {
+				return;
+			}
 			this.data.splice(this.data.indexOf(item), 1);
 		}
 
@@ -17,13 +28,14 @@ const GenericClasses = () => {
 		}
 	}
 
-	const textStorage = new DataStorage<string>();
+	// its also possible to use Uniontypes
+	const textStorage = new DataStorage();
 	textStorage.addItem("Max");
 	textStorage.addItem("Peter");
 	textStorage.removeItem("Peter");
 	console.log(textStorage.getItem());
 
-	const numberStorage = new DataStorage<number>();
+	const numberStorage = new DataStorage();
 	numberStorage.addItem(2);
 	console.log(numberStorage.getItem());
 
