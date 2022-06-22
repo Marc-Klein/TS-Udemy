c625a2ddd7e51e98e1303cf258894bc3ab85cadf
Marc Klein <maklein@abatgroup.de>
2022-06-22 15:36:03 +0200

    added template decorator

--- src/8-Decorators/moreUsefulDecorators/index.tsx
+++ src/8-Decorators/moreUsefulDecorators/index.tsx
@@ -8,16 +8,22 @@ const FirstClassDecorator = () => {
 		};
 	};
 
+	// with this Function we describe the decorator,
+	// we give to inputs, our template what we want to render and the id to look for
 	const TemplateDecorator = (template: string, hookID: string) => {
 		// _ Im aware of the argument but I dont use it
 		return (_: Function) => {
+			// like this we describe the how to get the id
 			const hookEl = document.getElementById(hookID);
 			if (hookEl) {
+				// here we check if we got an hookEL
+				// if  a hookEl is their we can create with innerHTML our template
 				hookEl.innerHTML = template;
+			} else {
+				throw new console.error("nos Element found!");
 			}
 		};
 	};
-
 	// @LoggerDecorator("logging Person")
 	@TemplateDecorator("<h1>Peter</h1>", "app")
 	class Person {
