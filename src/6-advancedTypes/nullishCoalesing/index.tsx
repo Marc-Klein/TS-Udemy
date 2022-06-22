0e18f51f4005594fad2234fc6b234e40ab09f233
Marc Klein <maklein@abatgroup.de>
2022-06-22 13:39:46 +0200

    removed typos

--- src/6-advancedTypes/nullishCoalesing/index.tsx
+++ src/6-advancedTypes/nullishCoalesing/index.tsx
@@ -4,6 +4,8 @@ const NullishCoalescing = () => {
 	// how to check for nullish values
 
 	const userInput = null;
+	// the double questionmark operator makes sure to set default
+	// this is also called nullishCoalescing
 	const storedData = userInput ?? "Default";
 
 	console.log(storedData);
